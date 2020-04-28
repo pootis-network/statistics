0e7403af38a50e2780406c1b193f6d640e6e56c1
Robert Fraser <robert.fraser@uqconnect.edu.au>
2020-04-28 21:11:48 +1000

    Merge URL commands into statistics

---
+++ lua/autorun/client/cl_urls.lua
@@ -0,0 +1,23 @@
+net.Receive('StatisticsOpenURL', function()
+    local url = net.ReadString()
+    gui.OpenURL(url)
+end)
+
+local advert_messages = {
+    'Like the server? Join our steam group by typing !steamgroup',
+    'Join our discord at https://discord.gg/UdMTckn (or type !discord)',
+    'Type !donate to open our donation page and help fund Fluffy Servers',
+    'Servers are expensive - donate to us & keep us alive.',
+    'All the cool kids are in our discord - type !discord to join us!',
+    'Thanks for playing on Fluffy Servers! Your support is appreciated <3',
+    'Friendly reminder to give our staff team love!',
+    'Join our community - type !discord & !steamgroup',
+    'Type @ before your message to contact any online staff',
+    'You can use !stats to check your playtime. You get respected ranks at 10, 24, 50, and 100 hours.',
+    'Remember to have fun and treat everyone nicely!',
+    'Join our discord at https://discord.gg/UdMTckn (or type !discord)' -- supposed to be twice
+}
+
+timer.Create('CommandAdverts', 200, 0, function()
+    LocalPlayer():ChatPrint(table.Random(advert_messages))
+end)
---
+++ lua/autorun/server/sv_urls.lua
@@ -0,0 +1,58 @@
+util.AddNetworkString('StatisticsOpenURL')
+
+local function openurl(ply, url)
+    net.Start('StatisticsOpenURL')
+        net.WriteString(url)
+    net.Send(ply)
+end
+
+local chat_commands = {
+    ['!steamgroup'] = function(ply)
+        openurl(ply, 'https://steamcommunity.com/groups/fluffyserversofficial')
+    end,
+
+    ['!discord'] = function(ply)
+        openurl(ply, 'https://discord.gg/UdMTckn')
+    end,
+
+    ['!donate'] = function(ply)
+        openurl(ply, 'https://fluffyservers.com/donate.html')
+    end,
+
+    ['!staffapply'] = function(ply)
+        openurl(ply, 'https://goo.gl/forms/zPU1EDrgpYVowEvu2')
+    end,
+
+    ['!apply'] = function(ply)
+        openurl(ply, 'https://goo.gl/forms/zPU1EDrgpYVowEvu2')
+    end,
+
+    ['!stats'] = function(ply)
+        if not ply.Playtime or not ply.JoinTime then return end
+        local seconds = ply.Playtime + (os.time() - ply.JoinTime)
+
+        if seconds > 10800 then
+            local hours = math.Round(seconds/3600, 1)
+            ply:ChatPrint('You have played for ' .. hours .. ' hours.')
+        else
+            local minutes = math.Round(seconds/60, 1)
+            ply:ChatPrint('You have played for' .. minutes .. ' minutes')
+        end
+    end,
+
+    ['!namecolor'] = function(ply)
+        ply:ConCommand('namecolor')
+    end,
+
+    ['!namecolour'] = function(ply)
+        ply:ConCommand('namecolor')
+    end,
+}
+
+hook.Add('PlayerSay', 'StatisticsChatCommands', function(ply, txt)
+    local message = string.lower(txt)
+    print(message)
+    if chat_commands[message] then
+        chat_commands[message](ply)
+    end
+end)
