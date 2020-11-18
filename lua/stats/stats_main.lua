d119213400cf898cb9c0242a37465e34b93a3ce2
Robert A Fraser <robert.fraser@uqconnect.edu.au>
2020-11-18 19:52:18 +1000

    Safer saving of statistics

--- lua/stats/stats_main.lua
+++ lua/stats/stats_main.lua
@@ -26,27 +26,27 @@ function STATS:UpdateStatistics()
     end
 end
 
+-- Save stats for all players every so often
 timer.Create('UpdateStatisticsTimer', STATS.UpdateTime or 120, 0, function()
     STATS:UpdateStatistics()
 end)
 
 -- Last-ditch attempt to save before shutdown
 -- This hook is probably called too late for queries to run, but it's worth a try
-hook.Add('ShutDown', 'SaveStatisticsOnShutdown', function() STATS:UpdateStatistics() end)
+hook.Add('ShutDown', 'SaveStatisticsOnShutdown', STATS.UpdateStatistics)
 
 -- Safer saving functions
-hook.Add('MapVoteWon', 'SaveStatisticsOnShutdown', function() STATS:UpdateStatistics() end)
-hook.Add('GameEnd', 'SaveStatisticsOnShutdown', function() STATS:UpdateStatistics() end)
+hook.Add('MapVoteWon', 'SaveStatisticsOnShutdown', STATS.UpdateStatistics)
+hook.Add('GameEnd', 'SaveStatisticsOnShutdown', STATS.UpdateStatistics)
 
 -- Update statistics for disconnecting players
---[[
+-- We have to be careful that all players here have valid IDs
 hook.Add('PlayerDisconnected', 'SaveStatisticsOnDisconnect', function(ply)
     if ply:IsBot() then return end
     if not ply:SteamID64() then return end
-
+    
     hook.Call('UpdateStatistics', nil, v)
 end)
-]]--
 
 -- Load component modules
 include('stats_playtime.lua')
